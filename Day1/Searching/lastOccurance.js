43c674fb9528ee2d4aa96f978a146162e1df0416
Avinash Yadav <ar5840@srmist.edu.in>
2025-04-21 15:02:04 +0530

    [Avinash Yadav]:Day1: Add find last occurrence of a target number using binary search

---
+++ Day1/Searching/lastOccurance.js
@@ -0,0 +1,27 @@
+// Given a sorted array with duplicates, return the first occurrence of a target number using binary search.
+// Input: firstOccurrence([1, 2, 2, 2, 3, 4], 2)  
+// Output: 1
+
+function firstOccurence(arr,target){
+    let ans=-1;
+    let s=0;
+    let e=arr.length-1;
+    while(s<=e){
+        let mid=Math.floor((s+e)/2);
+        if(arr[mid]<target){
+            s=mid+1;
+        }
+        else if(arr[mid]>target){
+            e=mid-1;
+        }
+        else{
+            ans=mid;
+            s=mid+1;
+            
+        }
+    }
+    return ans;
+}
+let arr=[1, 2, 2, 2, 3, 4];
+let res=firstOccurence(arr,2);
+console.log(res);
