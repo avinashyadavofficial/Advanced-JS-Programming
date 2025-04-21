0c1092c46557fd0208a95b081b7377b8df739c51
Avinash Yadav <ar5840@srmist.edu.in>
2025-04-21 10:36:32 +0530

    [Avinash Yadav]:Day1: Add deletion at End in LinkedList

---
+++ Day1/LinkedList/deleteAtEnd.js
@@ -0,0 +1,38 @@
+class Node{
+    constructor(data){
+        this.data=data;
+        this.next=null;
+    }
+}
+function print(head){
+    let ans="";
+    while(head!=null){
+        ans+=head.data;
+        if(head.next!=null){
+            ans+="->";
+        }
+        head=head.next;
+    }
+    console.log(ans);
+}
+function deleteAtEnd(head){
+    if(head==null || head.next==null){
+        return null;
+    }
+    let temp=head;
+    let prev=temp;
+    while(temp.next!=null){
+        prev=temp;
+        temp=temp.next;
+    }
+    let delNode=temp.data;
+    prev.next=null;
+    console.log(delNode);
+    return head;
+}
+let head=new Node(10);
+head.next=new Node(20);
+head.next.next=new Node(30);
+head.next.next.next=new Node(40);
+head=deleteAtEnd(head);
+print(head);
