4b6cea7e6c26055032afa77f2569af878dde32fa
Avinash Yadav <ar5840@srmist.edu.in>
2025-04-24 16:29:35 +0530

    [Avinash Yadav]:Day3: Add to implement hash table using objects

---
+++ Day3/HashTable/usingObject.js
@@ -0,0 +1,8 @@
+const hashTable = {};
+hashTable["name"] = "Avinash";
+console.log(hashTable["name"]); // "John"
+
+//Limitations:
+// Keys are only strings or symbols.
+// Can accidentally overwrite built-in properties.
+// No easy way to get the size
