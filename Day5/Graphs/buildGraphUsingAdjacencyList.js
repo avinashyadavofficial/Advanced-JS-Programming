a92be355224e938ffbf7e1b61ac5052274ce3eb5
Avinash Yadav <ar5840@srmist.edu.in>
2025-04-26 13:06:29 +0530

    [Avinash Yadav]:Day5: Modify building graph using adjacency List: add weighted graph

--- Day5/Graphs/buildGraphUsingAdjacencyList.js
+++ Day5/Graphs/buildGraphUsingAdjacencyList.js
@@ -7,15 +7,30 @@ class Graph{
             this.adjacencyList[vertex]=[];
         }
     }
+    //for unweighted
     addEdge(v1,v2){ //undirected graph so no direction so push in both
         this.adjacencyList[v1].push(v2);
         this.adjacencyList[v2].push(v1);
     }
+    // //for weighted
+    // addEdge(vertex1, vertex2, weight) {
+    //     this.adjacencyList[vertex1].push({node: vertex2, weight: weight});
+    //     this.adjacencyList[vertex2].push({node: vertex1, weight: weight}); // if undirected
+    //   }
     showConnections(){
         for(let vertex in this.adjacencyList){
             console.log(`${vertex} -> ${this.adjacencyList[vertex]}`);
         }
     }
+    //for weighted
+    // showConnections() {
+    //     for (let vertex in this.adjacencyList) {
+    //         const connections = this.adjacencyList[vertex]
+    //             .map(edge => `${edge.node}(${edge.weight})`)
+    //             .join(", ");
+    //         console.log(`${vertex} -> ${connections}`);
+    //     }
+    // }
 }
 const graph=new Graph();
 let vertex=['A','B','C'];
