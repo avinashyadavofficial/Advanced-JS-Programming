64bedb0330f5652317599e65ef21c294ca5d74d9
Avinash Yadav <ar5840@srmist.edu.in>
2025-05-03 16:09:06 +0530

    [Avinash Yadav]:Day10:Add : Promise API: resolve()

---
+++ Day10/resolve.js
@@ -0,0 +1,12 @@
+// Promise.resolve() – Instantly resolve a value
+Promise.resolve("Instant Win")
+  .then(value => console.log(value));
+//Instant Win
+
+Promise.resolve(5)
+  .then(a=>console.log(a));
+//5
+
+
+
+
